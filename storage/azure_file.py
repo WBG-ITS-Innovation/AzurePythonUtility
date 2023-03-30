b541c4de47571e7bd8a115d7ad7b38889e4f7cb1
Mert Ozdag <mert.ozdag@gmail.com>
2023-03-30 14:54:25 -0400

    file fixes

--- storage/azure_file.py
+++ storage/azure_file.py
@@ -5,25 +5,31 @@ from datetime import datetime, timedelta
 
 def normalize_email(s):
     s = s.replace(".","1")
-    s = s.replace("@","2")        
-    print(s)
+    s = s.replace("@","2")            
     return s
 
 def get_blob_service_client():
-  connect_str = os.getenv('STORAGE_CONNECTION_STRING')
+  connect_str = os.getenv("AZURE_TABLE_CS")
   blob_service_client = blob_service_client = BlobServiceClient.from_connection_string(connect_str)
   return blob_service_client
 
-def blob_upload_file(container_name, local_path, local_file_name):
+def blob_upload_file(container_name, local_path, local_file_name, email):
+  print(container_name)
+  print(local_file_name)
+  print(local_path)
   service_client = get_blob_service_client() 
-  container_client = service_client.get_container_client(container_name)
+  container_client = service_client.get_container_client('climate')
   if not container_client.exists():
     container_client.create_container()
   blob_client = container_client.get_blob_client(local_file_name)
   upload_file_path = os.path.join(local_path, local_file_name)
+  metadata = {'uploaded_by':'test', 'status':'UPLOADED'}
+  metadata["uploaded_by"] = email
+  print(metadata)
   with open(file=upload_file_path, mode="rb") as data:
-    blob_client.upload_blob(data, overwrite=True)
+    result = blob_client.upload_blob(data, overwrite=True, metadata=metadata)
   os.remove(upload_file_path)
+  return result
 
 def blob_download_file(container_name, local_path, blob_file_name):
   service_client = get_blob_service_client()
@@ -38,7 +44,20 @@ def blob_download_file(container_name, local_path, blob_file_name):
 def blob_list_container(container_name):
   service_client = get_blob_service_client() 
   container_client = service_client.get_container_client(container_name)
-  return container_client.list_blobs()
+  return container_client.list_blobs(include='metadata')
+
+def change_metadata(container_name, file_name, key, value):
+  service_client = get_blob_service_client()
+  blob_client = service_client.get_blob_client(container_name, file_name)
+  metadata = blob_client.get_blob_properties().metadata
+  metadata[key] = value
+  blob_client.set_blob_metadata(metadata)
 
 def delete_local_file(file):  
   os.remove(file)
+
+if __name__ == "__main__":
+  service_client = get_blob_service_client() 
+  container_client = service_client.get_container_client('climate')
+  print(container_client.exists())  
+  #service_client.create_container('climate')
