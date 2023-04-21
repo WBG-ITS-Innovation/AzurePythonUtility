14751e3723fe1ba85949c04ad15ec8ad0d6b0b47
Mert Ozdag <mert.ozdag@gmail.com>
2023-04-21 20:15:57 +0000

    convert to classes

--- storage/azure_file.py
+++ storage/azure_file.py
@@ -3,61 +3,60 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, generate_account_sas, ResourceTypes, AccountSasPermissions
 from datetime import datetime, timedelta
 
-def normalize_email(s):
-    s = s.replace(".","1")
-    s = s.replace("@","2")            
-    return s
 
-def get_blob_service_client():
-  connect_str = os.getenv("AZURE_TABLE_CS")
-  blob_service_client = blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-  return blob_service_client
+class AzureFile:
 
-def blob_upload_file(container_name, local_path, local_file_name, email):
-  print(container_name)
-  print(local_file_name)
-  print(local_path)
-  service_client = get_blob_service_client() 
-  container_client = service_client.get_container_client('climate')
-  if not container_client.exists():
-    container_client.create_container()
-  blob_client = container_client.get_blob_client(local_file_name)
-  upload_file_path = os.path.join(local_path, local_file_name)
-  metadata = {'uploaded_by':'test', 'status':'UPLOADED'}
-  metadata["uploaded_by"] = email
-  print(metadata)
-  with open(file=upload_file_path, mode="rb") as data:
-    result = blob_client.upload_blob(data, overwrite=True, metadata=metadata)
-  os.remove(upload_file_path)
-  return result
+  def normalize_email(s):
+      s = s.replace(".","1")
+      s = s.replace("@","2")            
+      return s
 
-def blob_download_file(container_name, local_path, blob_file_name):
-  service_client = get_blob_service_client()
-  blob_client = service_client.get_blob_client(container=container_name, blob=blob_file_name)
-  if not os.path.exists(local_path):
-    os.mkdir(local_path)
-  with open(file=os.path.join(local_path, blob_file_name), mode="wb") as sample_blob:
-    download_stream = blob_client.download_blob()
-    sample_blob.write(download_stream.readall())
-  return os.path.join(local_path, blob_file_name)
+  def get_blob_service_client(self):
+    blob_service_client = blob_service_client = BlobServiceClient.from_connection_string(self.cs)
+    return blob_service_client
 
-def blob_list_container(container_name):
-  service_client = get_blob_service_client() 
-  container_client = service_client.get_container_client(container_name)
-  return container_client.list_blobs(include='metadata')
+  def blob_upload_file(self, container_name, local_path, local_file_name, email):
+    print(container_name)
+    print(local_file_name)
+    print(local_path)
+    service_client = self.get_blob_service_client() 
+    container_client = service_client.get_container_client('climate')
+    if not container_client.exists():
+      container_client.create_container()
+    blob_client = container_client.get_blob_client(local_file_name)
+    upload_file_path = os.path.join(local_path, local_file_name)
+    metadata = {'uploaded_by':'test', 'status':'UPLOADED'}
+    metadata["uploaded_by"] = email
+    print(metadata)
+    with open(file=upload_file_path, mode="rb") as data:
+      result = blob_client.upload_blob(data, overwrite=True, metadata=metadata)
+    os.remove(upload_file_path)
+    return result
 
-def change_metadata(container_name, file_name, key, value):
-  service_client = get_blob_service_client()
-  blob_client = service_client.get_blob_client(container_name, file_name)
-  metadata = blob_client.get_blob_properties().metadata
-  metadata[key] = value
-  blob_client.set_blob_metadata(metadata)
+  def blob_download_file(self, container_name, local_path, blob_file_name):
+    service_client = self.get_blob_service_client()
+    blob_client = service_client.get_blob_client(container=container_name, blob=blob_file_name)
+    if not os.path.exists(local_path):
+      os.mkdir(local_path)
+    with open(file=os.path.join(local_path, blob_file_name), mode="wb") as sample_blob:
+      download_stream = blob_client.download_blob()
+      sample_blob.write(download_stream.readall())
+    return os.path.join(local_path, blob_file_name)
 
-def delete_local_file(file):  
-  os.remove(file)
+  def blob_list_container(self, container_name):
+    service_client = self.get_blob_service_client() 
+    container_client = service_client.get_container_client(container_name)
+    return container_client.list_blobs(include='metadata')
 
-if __name__ == "__main__":
-  service_client = get_blob_service_client() 
-  container_client = service_client.get_container_client('climate')
-  print(container_client.exists())  
-  #service_client.create_container('climate')
+  def change_metadata(self, container_name, file_name, key, value):
+    service_client = self.get_blob_service_client()
+    blob_client = service_client.get_blob_client(container_name, file_name)
+    metadata = blob_client.get_blob_properties().metadata
+    metadata[key] = value
+    blob_client.set_blob_metadata(metadata)
+
+  def delete_local_file(file):  
+    os.remove(file)
+
+  def __init__(self, connection_string) -> None:
+    self.cs = connection_string
--- storage/azure_queue.py
+++ storage/azure_queue.py
@@ -1,44 +1,39 @@
 from azure.storage.queue import QueueServiceClient, QueueClient, QueueMessage, StorageErrorCode
 import os, time
 
-def get_queue_client(queue_name):
-  try:
-    connect_str = os.getenv('STORAGE_CONNECTION_STRING')
-    print(connect_str)
-    queue_client = QueueClient.from_connection_string(connect_str, queue_name)
-    return queue_client
-  except Exception as ex:
-    print('Exception:')
-    print(ex)
+class AzureQueue:
 
-def get_queue(queue_name):
-  try:
-    client = get_queue_client(queue_name)
-    client.create_queue()
-  except Exception as ex:
-    if ex.error_code == StorageErrorCode.QUEUE_ALREADY_EXISTS:
+  def get_queue_client(self, queue_name):
+    try:
+      queue_client = QueueClient.from_connection_string(self.cs, queue_name)
+      return queue_client
+    except Exception as ex:
+      print('Exception:')
+      print(ex)
+
+  def get_queue(self, queue_name):
+    try:
+      client = self.get_queue_client(queue_name)
+      client.create_queue()
+    except Exception as ex:
+      if ex.error_code == StorageErrorCode.QUEUE_ALREADY_EXISTS:
+        return client
+    else: 
       return client
-  else: 
-    return client
 
-def send_message(queue_name, message):
-  queue_client = get_queue(queue_name)  
-  queue_client.send_message(message)  
+  def send_message(self, queue_name, message):
+    queue_client = self.get_queue(queue_name)  
+    queue_client.send_message(message)  
 
-def recieve_message(queue_name):
-  queue_client = get_queue(queue_name)
-  return queue_client.receive_message(visibility_timeout=6000)
+  def recieve_message(self, queue_name):
+    queue_client = self.get_queue(queue_name)
+    return queue_client.receive_message(visibility_timeout=6000)
 
-def delete_message(queue_name, message):
-  queue_client = get_queue(queue_name)
-  queue_client.delete_message(message)
+  def delete_message(self, queue_name, message):
+    queue_client = self.get_queue(queue_name)
+    queue_client.delete_message(message)
 
-if __name__ == "__main__":
-  queue_client = get_queue('embedding')
-  queue_client.send_message('test')
-  msg = queue_client.receive_message(visibility_timeout=6000)
-  print(msg)  
-  time.sleep(2)
-  msg2 = queue_client.receive_message(visibility_timeout=6000)
-  print(msg2)
+  def __init__(self, connection_string):
+    self.cs = connection_string
+
                     
